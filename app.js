d07ed0c7007d6f3d6e05c7ce46400578e6ae06ff
kool-teche <koolteche@gmail.com>
2024-05-05 08:50:49 +0530

    change port number

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { exec } = require('child_process');
 const app = express();
-const PORT = 8000;
+const PORT = 8080;
 
 app.use(express.static('public'));
 
